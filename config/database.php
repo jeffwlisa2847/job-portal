35b81dc555fafb4cae1cc51411c36e1bf25de1c0
jeffwlisa2847 <jeffwlisa2847@gmail.com>
2026-06-06 00:47:32 +0000

    fix: Change default database name to 'default' in configuration

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 return [
     'host'     => getenv('DB_HOST') ?: 'localhost',
     'port'     => getenv('DB_PORT') ?: '3306',
-    'dbname'   => getenv('DB_NAME') ?: 'job_portal',
+    'dbname'   => getenv('DB_NAME') ?: 'default',
     'username' => getenv('DB_USER') ?: 'root',
     'password' => getenv('DB_PASSWORD') ?: '',
 ];
